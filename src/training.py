37a84661dbbb4b703d9e9631278f4eb3c89e94b8
Qiyin <qiyin@stanford.edu>
2020-11-11 23:44:32 -0800

    add customized training_step function skeleton

--- src/training.py
+++ src/training.py
@@ -144,14 +144,63 @@ training_args = TrainingArguments(
     save_total_limit=10,
 )
 
-def compute_hybrid_loss(labels, outputs):
+def compute_hybrid_reward(labels, outputs):
     pass
 
+prev_reward = 0
 class CustomizeTrainer(Trainer):
-    def compute_loss(self, model, inputs):
-        labels = inputs.pop("labels")
-        outputs = models(**inputs)
-        return compute_hybrid_loss(outputs[0], labels)
+    # Override training step
+    def training_step(self, model: nn.Module, inputs: Dict[str, Union[torch.Tensor, Any]]) -> torch.Tensor:
+        """
+        Perform a training step on a batch of inputs.
+        Subclass and override to inject custom behavior.
+        Args:
+            model (:obj:`nn.Module`):
+                The model to train.
+            inputs (:obj:`Dict[str, Union[torch.Tensor, Any]]`):
+                The inputs and targets of the model.
+                The dictionary will be unpacked before being fed to the model. Most models expect the targets under the
+                argument :obj:`labels`. Check your model's documentation for all accepted arguments.
+        Return:
+            :obj:`torch.Tensor`: The tensor with training loss on this batch.
+        """
+        if hasattr(self, "_training_step"):
+            warnings.warn(
+                "The `_training_step` method is deprecated and won't be called in a future version, define `training_step` in your subclass.",
+                FutureWarning,
+            )
+            return self._training_step(model, inputs, self.optimizer)
+
+        model.train()
+        inputs = self._prepare_inputs(inputs)
+
+        if self.args.fp16 and _use_native_amp:
+            with autocast():
+                loss = self.compute_loss(model, inputs)
+        else:
+            loss = self.compute_loss(model, inputs)
+
+        if self.args.n_gpu > 1:
+            loss = loss.mean()  # mean() to average on multi-gpu parallel training
+
+        if self.args.gradient_accumulation_steps > 1:
+            loss = loss / self.args.gradient_accumulation_steps
+
+        reward = compute_hybrid_reward(labels, outputs)
+        global prev_reward
+        loss *= (reward - prev_reward)
+        prev_reward = reward
+
+        if self.args.fp16 and _use_native_amp:
+            self.scaler.scale(loss).backward()
+        elif self.args.fp16 and _use_apex:
+            with amp.scale_loss(loss, self.optimizer) as scaled_loss:
+                scaled_loss.backward()
+        else:
+            loss.backward()
+
+        return loss.detach()
+
 
 # instantiate trainer
 trainer = Trainer(

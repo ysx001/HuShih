2b6f20c392c585237fce47c5b99b8a5d67db9e87
Sarah Xu <saraxu@amazon.com>
2020-11-12 21:27:45 -0800

    Checking gpu

--- src/training.py
+++ src/training.py
@@ -262,6 +262,20 @@ if __name__ == '__main__':
 
     LOG.info("Test files saved to path {}".format(lcsts.test_merged_csv))
 
+    print('__Python VERSION:', sys.version)
+    print('__pyTorch VERSION:', torch.__version__)
+    print('__CUDA VERSION')
+    from subprocess import call
+    # call(["nvcc", "--version"]) does not work
+    print('__CUDNN VERSION:', torch.backends.cudnn.version())
+    print('__Number CUDA Devices:', torch.cuda.device_count())
+    print('__Devices')
+    call(["nvidia-smi", "--format=csv", "--query-gpu=index,name,driver_version,memory.total,memory.used,memory.free"])
+    print('Active CUDA Device: GPU', torch.cuda.current_device())
+
+    print('Available devices ', torch.cuda.device_count())
+    print('Current cuda device ', torch.cuda.current_device())
+    
     # Load tokenizer
     try:
         with torch.cuda.device(1):
